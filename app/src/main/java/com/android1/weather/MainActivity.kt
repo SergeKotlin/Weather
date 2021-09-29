c89796e5e1f57c576cbfa84d3681ae5e5b11cf8c
Serge Kotlin <jobsfolder@yandex.ru>
2021-09-29 23:54:43 +0300

    Initial commit

---
+++ app/src/main/java/com/android1/weather/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.android1.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android1.weather.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
